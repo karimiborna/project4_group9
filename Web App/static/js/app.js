9f44f725aad09e013a1f175c2dab0405c38aea41
Wheezyotter <jacobpohs@gmail.com>
2023-08-14 18:37:59 -0700

    working on random sampler

--- Web App/static/js/app.js
+++ Web App/static/js/app.js
@@ -7,4 +7,19 @@ function createSample() {
     randomInfo.text("Test: " + testValue);
 }
 
+function displaySampleInfo() {
+   let sampleAge = d3.select("#sample-metadata").append("h5");
+   let sampleSex = d3.select("#sample-metadata").append("h5");
+   let sampleChestPain = d3.select("#sample-metadata").append("h5");
+   let sampleRestingBP = d3.select("#sample-metadata").append("h5");
+   let sampleChol = d3.select("#sample-metadata").append("h5");
+   let sampleFastingBS = d3.select("#sample-metadata").append("h5");
+   let sampleRestECG = d3.select("#sample-metadata").append("h5");
+   let sampleMaxHR = d3.select("#sample-metadata").append("h5");
+   let sampleExerciseAng = d3.select("#sample-metadata").append("h5");
+   let sampleOldPeak = d3.select("#sample-metadata").append("h5");
+   let sampleSTSlope = d3.select("#sample-metadata").append("h5");
+   let HeartDiesease = d3.select("#sample-metadata").append("h5");
+}
+
 random_sample_buttom.on('click', createSample);
