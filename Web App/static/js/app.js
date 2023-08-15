cbda1bc10c367a2b936fbcafed14824f197938af
Wheezyotter <jacobpohs@gmail.com>
2023-08-14 19:16:42 -0700

    progress on random generator

--- Web App/static/js/app.js
+++ Web App/static/js/app.js
@@ -1,25 +1,39 @@
 let random_sample_buttom = d3.select("#random-person");
 let randomInfo = d3.select("#sampleInfo");
 
+let sampleAge = d3.select("#sample-metadata").append("h5");
+let sampleSex = d3.select("#sample-metadata").append("h5");
+let sampleChestPain = d3.select("#sample-metadata").append("h5");
+let sampleRestingBP = d3.select("#sample-metadata").append("h5");
+let sampleChol = d3.select("#sample-metadata").append("h5");
+let sampleFastingBS = d3.select("#sample-metadata").append("h5");
+let sampleRestECG = d3.select("#sample-metadata").append("h5");
+let sampleMaxHR = d3.select("#sample-metadata").append("h5");
+let sampleExerciseAng = d3.select("#sample-metadata").append("h5");
+let sampleOldPeak = d3.select("#sample-metadata").append("h5");
+let sampleSTSlope = d3.select("#sample-metadata").append("h5");
+let HeartDiesease = d3.select("#sample-metadata").append("h5");
+
+function init() {
+    createSample();
+}
+
 function createSample() {
-    testValue = Math.random() * 10
-    console.log(testValue);
-    randomInfo.text("Test: " + testValue);
+    sampleAge.text("Age: " + getRndInteger(40,100));
+    sampleSex.text("Sex: " + getRndInteger(40,100));
+    sampleChestPain.text("Chest Pain: " + getRndInteger(40,100));
+    sampleRestingBP.text("Resting BP: " + getRndInteger(40,100));
+    sampleChol.text("Cholesterol: " + getRndInteger(40,100));
+    sampleFastingBS.text("Fasting BS: " + getRndInteger(40,100));
+    sampleRestECG.text("Sample Age: " + getRndInteger(40,100));
+    sampleMaxHR.text("Sample Age: " + getRndInteger(40,100));
+    sampleExerciseAng.text("Sample Age: " + getRndInteger(40,100));
+    sampleOldPeak.text("Sample Age: " + getRndInteger(40,100));
+    sampleSTSlope.text("Sample Age: " + getRndInteger(40,100));
+    HeartDiesease.text("Sample Age: " + getRndInteger(40,100));
 }
 
-function displaySampleInfo() {
-   let sampleAge = d3.select("#sample-metadata").append("h5");
-   let sampleSex = d3.select("#sample-metadata").append("h5");
-   let sampleChestPain = d3.select("#sample-metadata").append("h5");
-   let sampleRestingBP = d3.select("#sample-metadata").append("h5");
-   let sampleChol = d3.select("#sample-metadata").append("h5");
-   let sampleFastingBS = d3.select("#sample-metadata").append("h5");
-   let sampleRestECG = d3.select("#sample-metadata").append("h5");
-   let sampleMaxHR = d3.select("#sample-metadata").append("h5");
-   let sampleExerciseAng = d3.select("#sample-metadata").append("h5");
-   let sampleOldPeak = d3.select("#sample-metadata").append("h5");
-   let sampleSTSlope = d3.select("#sample-metadata").append("h5");
-   let HeartDiesease = d3.select("#sample-metadata").append("h5");
-}
-
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+  }
 random_sample_buttom.on('click', createSample);
