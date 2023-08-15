a9c899880436d76895d8a4a1a2d2b0790dceb4a1
Wheezyotter <jacobpohs@gmail.com>
2023-08-14 22:03:42 -0700

    flask setup

---
+++ Web App/app.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///titanic.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(autoload_with=engine)
+
+# Save reference to the table
+Passenger = Base.classes.passenger
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+    )
+
+if __name__ == '__main__':
+    app.run(debug=True)
