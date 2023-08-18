3ad3b4e6bb82aac9357c9f480eead2b09752c248
Borna Karimi <borna.karimi@gmail.com>
2023-08-17 19:43:11 -0700

    updated web app files, only problem left is nn_models not returning right preds

--- Web App/static/js/app.js
+++ Web App/static/js/app.js
@@ -40,9 +40,9 @@ let formChestPain = d3.select("#hidden-chest-pain");
 let formRestingBP = d3.select("#hidden-resting-BP");
 let formChol = d3.select("#hidden-chol");
 let formFastingBS = d3.select("#hidden-fast-BS");
-let formRestECG = d3.select("#hiddenrest-ECG");
-let formMaxHR = d3.select("#hiddenmax-HR");
-let formExerciseAng = d3.select("#hiddenexer-ang");
+let formRestECG = d3.select("#hidden-rest-ECG");
+let formMaxHR = d3.select("#hidden-max-HR");
+let formExerciseAng = d3.select("#hidden-exer-ang");
 let formOldPeak = d3.select("#hidden-oldpeak");
 let formSTSlope = d3.select("#hidden-slop-ST");
 
